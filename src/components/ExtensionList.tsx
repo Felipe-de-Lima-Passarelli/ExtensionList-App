42651eb8301901bf6d71d4f0ac5415960ed0fb18
Felipe de Lima Passarelli <malamdorox@gmail.com>
2026-01-05 16:34:04 -0300

    Update ExtensionList.tsx

--- src/components/ExtensionList.tsx
+++ src/components/ExtensionList.tsx
@@ -2,6 +2,7 @@
 
 //Next
 import Image from "next/image";
+import { useState } from "react";
 
 //Data
 import data from "../data/data.json";
@@ -18,10 +19,17 @@ const ExtensionList = ({
   activeButton,
   inactiveButton,
 }: extensionListProps) => {
+  const [dtBase, setDtBase] = useState(data);
+
+  //Removing datas
+  const handleRemove = (name: string) => {
+    setDtBase((prev) => prev.filter((item) => item.name !== name));
+  };
+
   return (
     <>
       {allButton &&
-        data.map((item, index) => (
+        dtBase.map((item, index) => (
           <div
             key={index}
             className="border bg-[#1F2535] border-gray-600 rounded-xl p-4  min-w-50 flex flex-col"
@@ -34,8 +42,11 @@ const ExtensionList = ({
               </div>
             </div>
             <div className="flex flex-row justify-between items-center mt-auto pt-15">
-              <button className="border py-2 px-4 rounded-xl cursor-pointer">
-                Remove
+              <button
+                onClick={() => handleRemove(item.name)}
+                className="border py-2 px-4 rounded-xl cursor-pointer"
+              >
+                Remover
               </button>
               {item.isActive ? (
                 <p className="text-green-600">Active</p>
@@ -46,7 +57,7 @@ const ExtensionList = ({
           </div>
         ))}
       {activeButton &&
-        data
+        dtBase
           .filter((item) => item.isActive === true)
           .map((item, index) => (
             <div
@@ -73,7 +84,7 @@ const ExtensionList = ({
             </div>
           ))}
       {inactiveButton &&
-        data
+        dtBase
           .filter((item) => item.isActive === false)
           .map((item, index) => (
             <div
